50e8d9b098f7b6359eb267c149d11987161db6f9
Celine Muhoracyeye <celine.muhoracyeyee@gmail.com>
2026-02-18 00:00:47 -0700

    Add files via upload

---
+++ restfullApiAssignment/src/main/java/auca/ac/rw/restfullApiAssignment/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package auca.ac.rw.restfullApiAssignment.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import auca.ac.rw.restfullApiAssignment.modal.ecommerce.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+}
